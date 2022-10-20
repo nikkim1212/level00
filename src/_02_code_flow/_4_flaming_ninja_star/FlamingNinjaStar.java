1147a18bac187cfee16939c1b9f445b463f5db62
nikkim1212 <115122803+nikkim1212@users.noreply.github.com>
2022-10-20 01:43:11 +0000

    Update FlamingNinjaStar.java

--- src/_02_code_flow/_4_flaming_ninja_star/FlamingNinjaStar.java
+++ src/_02_code_flow/_4_flaming_ninja_star/FlamingNinjaStar.java
@@ -10,35 +10,41 @@ public class FlamingNinjaStar {
 		int flameSize = 130;		//the length of the flaming arms
 		
 		// Make a new robot, and set it's pen down.
+Robot robo = new Robot();
 
+robo.penDown();
 		// Set the robot speed to 100
-		
+		robo.setSpeed(100);
 		// Set the robot window size to 800 x 800
-
+robo.setWindowSize(800, 800);
 		//  COUNT. Make another int variable to count how many times the loop has repeated
 		//         Set its start value to zero.
-		
+		int count = 0;
 	       //  LOOP. Start a while loop to repeat all of the code below ONE time (we will change this later)
-
+while(count < 25) {
 			   // TURN RIGHT     Turn the robot 1/8 of a circle (hint: 360 degrees will turn a full circle)
-		
+		robo.turn(360/8);
 			   // MOVE           Move the robot 64 pixels
-
+robo.move(64);
 			   // TURN LEFT      Turn the robot 40 degrees to the LEFT. (Negative numbers will turn the robot counter-clockwise.)
-			
+			robo.turn(-40);
 			   // DRAW FLAME     Move the robot the distance in the variable flameSize
-			
+			robo.setPenColor(Color.yellow);
+			robo.move(flameSize);
 				//               Turn the robot 170 degrees
+			robo.turn(170);
 			
 				//               Move the robot the distance in the variable flameSize (again)
-			
+		
+			robo.move(flameSize);
+			robo.setPenColor(Color.BLACK);
 			   // TURN RIGHT     Turn the robot 64 degrees to the right
-			
+			robo.turn(64);
 				// MOVE         Move the robot the distance in the variable baseSize
-		
+		robo.move(baseSize);
 				//  INCREASE COUNT. Increase the count by 1
-		
-		//  End the while loop here
+		count += 1;
+}//  End the while loop here
 		
 		// TEST   Run the program. Check that your shape is the same as the first picture in the recipe. 
 		//        This is one arm of the ninja star.
