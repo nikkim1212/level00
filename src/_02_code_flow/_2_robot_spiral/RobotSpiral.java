3533009838475e92a05d06ed39f5b341b87c4dc0
nikkim1212 <115122803+nikkim1212@users.noreply.github.com>
2022-10-20 01:41:43 +0000

    Update RobotSpiral.java

--- src/_02_code_flow/_2_robot_spiral/RobotSpiral.java
+++ src/_02_code_flow/_2_robot_spiral/RobotSpiral.java
@@ -12,31 +12,34 @@ public class RobotSpiral {
 	public static void main(String[] args) {
 		
 		// Create a new Robot
-		
+		Robot robo = new Robot();
+		Robot robo2 = new Robot();
 		// Set your robot's pen down 
+		robo.penDown();
 		
 		// SPEED. Set the robot to go at max speed (100)
-
+robo.setSpeed(100);
 		// COUNT. Create an int variable that will count how many lines of the spiral we have drawn.
 			//        Start its value as zero.
-
+int count = 0;
 		// LOOP. Start a while loop to repeat the COLOR, DRAW, TURN, and COUNT code below until 50 lines have been drawn
-
+while (count <50) {
 			// COLOR.   Have the robot set a random pen color:      setRandomPenColor()
-
+robo.setRandomPenColor();
 			// DRAW.    Move the robot (5*count) pixels
+robo.move(5*count);
 			//          count is the name of the variable you created earlier
 
 			// TURN.    Turn the robot (360/7) degrees to the right
-
+robo.turn(360/7);
 			// COUNT.   Increase the count of how many lines have been drawn so far ( count+=1 )
-
+count += 1;
 			// Change the robot pen width to the current value of the count variable 
-
+robo.setPenWidth(count);
 
 		//  End the while loop here
 
-		
+}
 		// Run the program. 
 		// Check the pattern against the picture in the recipe. If it matches, you are done!
 		
