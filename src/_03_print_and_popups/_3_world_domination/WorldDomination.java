4d0bae9b8b04fee55299ec84b56084e46902112e
nikkim1212 <115122803+nikkim1212@users.noreply.github.com>
2022-10-27 01:34:31 +0000

    Update WorldDomination.java

--- src/_03_print_and_popups/_3_world_domination/WorldDomination.java
+++ src/_03_print_and_popups/_3_world_domination/WorldDomination.java
@@ -8,11 +8,15 @@ public class WorldDomination {
 	public static void main(String[] args) {
 		
 		// 1. Ask the user if they know how to write code.
-
+String code =JOptionPane.showInputDialog("Do you know how to write code: ");
 		// 2. If they say "yes", tell them they will rule the world.
-
+if(code.equals("yes")) {
+	JOptionPane.showMessageDialog(null, "You will rule the world!");
+}
 		// 3. Otherwise, tell them to sign up for classes at The League.
-
+else {
+	JOptionPane.showMessageDialog(null, "sign up for classes at The League");
+}
 	}
 }
 
