2dcc7dc85f33a4aaea76d649d3514a3ca8f78199
nikkim1212 <115122803+nikkim1212@users.noreply.github.com>
2022-11-03 01:23:46 +0000

    Update AwesomeOrNot.java

--- src/_03_print_and_popups/_5_awesome_or_not/AwesomeOrNot.java
+++ src/_03_print_and_popups/_5_awesome_or_not/AwesomeOrNot.java
@@ -2,35 +2,58 @@ package _03_print_and_popups._5_awesome_or_not;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class AwesomeOrNot {
 
 	// 1. Make a main method that includes everything below
-
+public static void main(String[] args) {
 	
 		Random ran = new Random();    //This will be used below to make a random number. 
 		
 		// 2. Make a variable that will hold a random whole number
 	
 		// 3. Set your variable equal to a positive number less than 4 using     ran.nextInt(4); 
-	
+	ran.nextInt(4);
 		// 3. Print your variable to the console
-	
+	System.out.println(ran.nextInt(4));
 		// 4. Get the user to enter something that they think is awesome
-	
+	String userInput = JOptionPane.showInputDialog("What do you think is awsome? ");
 		// 5. If your variable is  0
-	
-			// -- tell the user whatever they entered is awesome!
-	
+	if(ran.nextInt(4)==0) {
+		// -- tell the user whatever they entered is awesome!
+		
+		JOptionPane.showMessageDialog(null, "That is awesome! ");
+	}
 		// 6. If your variable is  1
-	
+	if(ran.nextInt(4)==1) {
 			// -- tell the user whatever they entered is ok.
-	
+		JOptionPane.showMessageDialog(null, "That is okay ");
+		
+	}
 		// 7. If your variable is  2
-	
+	if(ran.nextInt(4)==2) {
 			// -- tell the user whatever they entered is boring.
-	
+		JOptionPane.showMessageDialog(null, "That is boring ");
+	}
 		// 8. If your variable is  3
+	if(ran.nextInt(4)==3) {
+		// -- invent your own message to give to the user (be nice).
+		JOptionPane.showMessageDialog(null, "I would have never thought of that! ");
+	}
+}
 	
-			// -- invent your own message to give to the user (be nice).
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
